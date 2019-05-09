cf1249a547e4c0d63ea37a3d62b9061225b23234
jason langford <jasonlangford15@yahoo.com>
2019-05-09 10:05:44 -0600

    wut-up

---
+++ src/Components/Practice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Practice extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World....I hope you don't explode</h1>
+      </div>
+    )
+  }
+}
